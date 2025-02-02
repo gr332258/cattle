a606a7261dd7d439fb5a2f2e890b08aec9991a04
gr332258 <gr332258@graded.br>
2025-02-02 12:53:52 -0300

    First Draft

---
+++ scanner.js
@@ -0,0 +1,30 @@
+import QrScanner from './lib/qr-scanner.min.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const videoElement = document.getElementById('qr-video');
+    const outputElement = document.getElementById('output');
+    const scanButton = document.getElementById('scan-button');
+
+    let scanner;
+
+    scanButton.addEventListener('click', async () => {
+        if (!scanner) {
+            scanner = new QrScanner(videoElement, (result) => {
+                // Show scanned data
+                outputElement.textContent = `Scanned Data: ${result}`;
+                console.log(result);
+                scanner.stop(); // Stop scanning after QR code
+                videoElement.style.display = 'none'; //stop vid
+            });
+        }
+
+        try {
+            videoElement.style.display = 'block'; // Show the video element
+            await scanner.start();
+        } catch (error) {
+            outputElement.textContent = 'Error: Unable to access camera.';
+            console.error(error);
+        }
+    });
+});
+
